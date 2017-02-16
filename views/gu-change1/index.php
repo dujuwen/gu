371839f8458f998e8b8acaca91be300979832c65
junwendu <381343736@qq.com>
2017-02-16 13:45:18 +0800

    界面简单调整

--- views/gu-change1/index.php
+++ views/gu-change1/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('添加', ['create'], ['class' => 'btn btn-success']) ?>
+        <?php //echo Html::a('添加', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
--- views/gu-monitor/view.php
+++ views/gu-monitor/view.php
@@ -7,16 +7,15 @@ use yii\widgets\DetailView;
 /* @var $model app\models\GuMonitor */
 
 $this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Gu Monitors', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => '添加监视数据', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="gu-monitor-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('继续添加', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('更新', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('删除', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
