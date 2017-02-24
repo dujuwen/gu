5759621ff70b6723591416e5d7b52213da65c4be
junwendu_for_windows <381343736@qq.com>
2017-02-24 23:04:45 +0800

    开启简化访问模式

--- config/web.php
+++ config/web.php
@@ -42,7 +42,7 @@ $config = [
         'db' => require(__DIR__ . '/db.php'),
         'urlManager' => [
             'enablePrettyUrl' => true,
-            'showScriptName' => false,
+            'showScriptName' => true,
             'rules' => [
             ],
         ],
